a4028f55e0fd5110c37d28eb9c188ea21f494dc8
anisssoudki <aniss.marrouch@gmail.com>
2020-11-02 10:00:52 -0700

    images in grid style

--- pics/pics/src/components/ImageList.js
+++ pics/pics/src/components/ImageList.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const ImageList = (props) => {
-   const images = props.images.map((image) =>{
-       return <img src={image.urls.regular}/>
+   const images = props.images.map(({description, id, urls}) =>{
+       return <img  style={{display: 'grid'}, {width: '250px'}} alt={description} key={id} src={urls.regular}/>
    })
-   
+
 return <div>{images}</div>;
 
 };
